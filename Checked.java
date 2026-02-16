a1a1cb9b086ba2a5ae45bca5c9e6d2446625ed54
Huzaif Ibrahim <i.huzaif.ibrahim@gmail.com>
2026-02-16 15:38:40 +0530

    Checked and Unchecked

---
+++ Checked.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+// class Checked{
+//     public static void main(String[] args) throws IOException {
+//         FileReader file = new FileReader("D:\\Java\\hello.txt");
+//         BufferedReader fileInput = new BufferedReader(file);
+
+//         for(int counter=0; counter<3; counter++){
+//             System.out.println(fileInput.readLine());
+//         }
+//         fileInput.close();
+//     }
+// }
+class Checked{
+    public static void main(String[] args) {
+        try{
+            FileReader file = new FileReader("D:\\Java\\hello.txt");
+            BufferedReader fileInput = new BufferedReader(file);
+
+            for(int counter=0; counter<3; counter++){
+                System.out.println(fileInput.readLine());
+            }
+            fileInput.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found "+ e.getMessage());
+        } catch(IOException e){
+            System.out.println("Error reading file " + e.getMessage());
+        }
+    }
+}
---
+++ Unchecked.java
@@ -0,0 +1,7 @@
+class Unchecked{
+    public static void main(String[] args){
+        int x = 0;
+        int y = 10;
+        int z = y/x;
+    }
+}
