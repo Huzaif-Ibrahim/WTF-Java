4542514c91e8525674438daa6c2c670c640959ee
Huzaif Ibrahim <i.huzaif.ibrahim@gmail.com>
2026-02-19 15:11:40 +0530

    Add program

---
+++ StaticBinding.java
@@ -0,0 +1,9 @@
+class Dog{ 
+    private void eat(){ 
+        System.out.println("Dog is eating..."); 
+    } 
+    public static void main(String args[]){ 
+        Dog d1=new Dog(); 
+        d1.eat(); 
+    } 
+} 
